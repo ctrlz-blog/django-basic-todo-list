1f29e89e3eac542c9f7e276f9571d96c8885d859
Alice Campkin <alice.campkin@gmail.com>
2022-07-23 21:06:51 +0100

    Part 4: add attributes to form inputs

--- todo/forms.py
+++ todo/forms.py
@@ -7,3 +7,14 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["name"]
+
+    name = forms.CharField(
+        label=False,
+        widget=forms.TextInput(
+            attrs={
+                "aria-label": "name",
+                "placeholder": "What do you need to do?",
+                "class": "form-control form-control-lg inline-block",
+            }
+        ),
+    )
